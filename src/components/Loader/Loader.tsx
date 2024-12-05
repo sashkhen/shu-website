1fb367157a452f8f2258f5612f0eae394dc31a9a
Sasha Diachenko <sashkhen056@gmail.com>
2024-12-05 22:23:18 +0100

    add loader and loading state for page

---
+++ src/components/Loader/Loader.tsx
@@ -0,0 +1,23 @@
+import clsx from "clsx";
+
+import { DivProps } from "@/types/base";
+
+import styles from "./Loader.module.scss";
+
+type BaseProps = Omit<DivProps, "data-testid" | "ref">;
+
+export type LoaderProps = BaseProps & {};
+
+const Loader: React.FC<LoaderProps> = ({ className, ...props }) => {
+  return (
+    <div className={clsx(styles.root, className)} {...props}>
+      <div className={styles.dots}>
+        {Array.from(Array(3)).map((_, i) => (
+          <div key={i} className={styles.dot} />
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default Loader;
