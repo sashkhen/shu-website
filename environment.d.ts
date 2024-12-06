3e2fac2844da13cd9f6a01121b6b30b9405b8336
Sasha Diachenko <sashkhen056@gmail.com>
2024-12-06 02:27:47 +0100

    update env vars

--- environment.d.ts
+++ environment.d.ts
@@ -1,6 +1,5 @@
 declare namespace NodeJS {
   export interface ProcessEnv {
-    readonly ENV_VARIABLE: string;
-    readonly NEXT_PUBLIC_ENV_VARIABLE: string;
+    readonly NEXT_PUBLIC_BASE_URL: string;
   }
 }
