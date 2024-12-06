efea7efe37666a642091317772fc86458051d46d
Sasha Diachenko <sashkhen056@gmail.com>
2024-12-06 02:07:32 +0100

    add env vars

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,6 +9,7 @@ export default async function Home() {
     <div className={styles.root}>
       <Background className={styles.background} />
       <Hero className={styles.hero} />
+      <p>{`server: ${process.env.ENV_VARIABLE}, client: ${process.env.NEXT_PUBLIC_ENV_VARIABLE}`}</p>
       <ThemeConfigurator className={styles.themeConfig} />
     </div>
   );
--- src/components/Experiences/Experiences.tsx
+++ src/components/Experiences/Experiences.tsx
@@ -35,6 +35,7 @@ const Experiences: React.FC<ExperiencesProps> = ({ className, ...props }) => {
             endDate={experience.endDate}
             stack={experience.techStack}
           >
+            <p>{`server: ${process.env.ENV_VARIABLE}, client: ${process.env.NEXT_PUBLIC_ENV_VARIABLE}`}</p>
             <FormattedMessage id={`experiences.[${i}].description`} />
           </Experience>
         ))
